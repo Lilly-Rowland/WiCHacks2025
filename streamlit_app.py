f86ea538e8080968b05a82e84158ccbdd02e0c74
Lilly <lgr4641@rit.edu>
2025-03-01 23:38:11 -0500

    updated stremalit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,22 @@ import cv2
 import numpy as np
 from PIL import Image
 from detect_pose import process_frame
+from seat_calibration2 import seat_calibration
 
 def main():
+    calibration_start_time = None
+    previous_hip_x = None
+    catch_calibrated = False
+    finish_calibrated = False
+    catch_x_position = None
+    finish_x_position = None
+    catch_calibration_time = None
+    cap = cv2.VideoCapture(0)
+    calibrated = False
+
     st.title("Posture Detection")
     st.write("Press 'Stop' to exit the video stream.")
 
-    catch_range_max = 0.6
-    finish_range_min = 0.8
-
-    cap = cv2.VideoCapture(0)
     stframe = st.empty()
 
     stop_button = st.button("Stop")
@@ -22,8 +29,14 @@ def main():
         if not success:
             break
 
-        frame = process_frame(frame, catch_range_max, finish_range_min)
-
+        if not (catch_calibrated and finish_calibrated):
+            frame, calibration_start_time, previous_hip_x, catch_calibrated, finish_calibrated, catch_x_position, finish_x_position, catch_calibration_time = seat_calibration(
+            frame, calibration_start_time, previous_hip_x, catch_calibrated, finish_calibrated, catch_x_position, finish_x_position, catch_calibration_time
+            )
+        else:
+            stroke_length = finish_x_position - catch_x_position
+            frame = process_frame(frame, (catch_x_position + stroke_length*.1), (finish_x_position-stroke_length*.1))
+       
         # Convert the frame to RGB format
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(frame)
